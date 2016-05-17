129490709e8d95d956e7b75341513e45636c1bb4
Yuki Koya(zipper) <ykky@paperboy.co.jp>
2016-05-17 12:58:12 +0900

    最大公約数を使って解くように修正した

--- 035/a.rb
+++ 035/a.rb
@@ -3,22 +3,10 @@
 w,h = STDIN.gets.split.map{|n| n.to_i}
 
 class Aspect
-  def initialize(w, h)
-    raise "invalide wide #{w}"   if h > w || w > 100000
-    raise "invalide height #{h}" if h < 1
-
-    @w = w
-    @h = h
-  end
-
-  def aspect
-    if @w % 16 == 0 && @h % 9 == 0
-      "16:9"
-    else
-      "4:3"
-    end
+  def self.ratio(w, h)
+    gcd = w.gcd(h)
+    "#{w/gcd}:#{h/gcd}"
   end
 end
 
-a = Aspect.new(w,h)
-puts a.aspect
+puts Aspect.ratio(w, h)
