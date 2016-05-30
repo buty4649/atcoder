796f93050bf95a961fc5d936b5d00496febbad20
Yuki Koya(zipper) <ykky@paperboy.co.jp>
2016-05-30 22:30:05 +0900

    Aのspecを追加

--- 038/spec/a_spec.rb
+++ 038/spec/a_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 t = AtCoderTest.new(__FILE__)
 
 describe t.name do
-#  it { expect(t.exec("<Question>")).to eq("<Answer>") }
+  it { expect(t.exec("ICEDT")).to eq("YES") }
+  it { expect(t.exec("MUGICHA")).to eq("NO") }
+  it { expect(t.exec("OOLONGT")).to eq("YES") }
+  it { expect(t.exec("T")).to eq("YES") }
+  it { expect(t.exec("TEA")).to eq("NO") }
 end
