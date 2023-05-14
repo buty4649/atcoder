3b15d59eeede9c1c75f72c173a2236bf2ce1353f
buty <buty4649@gmail.com>
2023-05-14 14:42:06 +0900

    よりシンプルに

--- abc301/d/main.rb
+++ abc301/d/main.rb
@@ -3,22 +3,17 @@
 s = gets.chomp
 n = gets.to_i
 
-# ?を0に置き換えて最小値を計算しておく
-answer = s.gsub('?', '0').to_i(2)
-
-# 最小値がnを超えていたら-1
-if answer > n
-  puts '-1'
-  exit
-end
-
-# ?を1に置き換えながらn以下の最大値を探す
-result = s.chars.each_with_index.inject(answer) do |sum, (c, i)|
-  if c != '?' || (sum | 1 << (s.size - i - 1)) > n
-    sum
+# ?を1に置き換えながら最大値を探す
+while s.index('?')
+  # 先頭だけ1にしてみてnを超えていないか判定
+  if s.sub('?', '1').gsub('?', '0').to_i(2) > n
+    # 超えていたら0
+    s.sub!('?', '0')
   else
-    sum | 1 << (s.size - i - 1)
+    # 超えていなかったら1
+    s.sub!('?', '1')
   end
 end
 
-puts result
+answer = s.to_i(2)
+puts answer > n ? '-1' : answer
