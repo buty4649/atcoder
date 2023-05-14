894be415be1cb47c5898ee69a43b98110d510573
buty <buty4649@gmail.com>
2023-05-14 14:01:06 +0900

    answer

--- abc298/d/main.rb
+++ abc298/d/main.rb
@@ -1,21 +1,24 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
+m = 998_244_353
 _q = gets
 
-s = 1
-d = 1
+s = [1]
+n = 1
+i = 0
 
 while line = gets&.chomp
-  (q, x) = line.split
+  (q, x) = line.split.map(&:to_i)
   case q
-  when '1'
-    # 末尾に追加
+  when 1
+    # 末尾に追加=10倍して足す
     s << x
-  when '2'
-    # 先頭の数字を削除
-    t += 1
-  when '3'
-    n = s[t..].to_i
-    puts n % 998_244_353
+    n = ((n * 10) + x) % m
+  when 2
+    # 先頭の数字を削除(=桁数を減らす)
+    n = (n - s[i].to_i * 10.pow(s.size - i - 1, m)) % m
+    i += 1
+  when 3
+    puts n
   end
 end
