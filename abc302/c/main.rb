c4c95a0e19acaedd9288b662670e24606f92d079
buty <buty4649@gmail.com>
2023-05-18 05:54:48 +0900

    abc302 cを解いた

--- abc302/c/main.rb
+++ abc302/c/main.rb
@@ -4,7 +4,19 @@ n, m = gets.split.map(&:to_i)
 s = n.times.map { gets.chomp }
 
 def diff(left, right, m)
-  m.times.sum do |j|
-    left[j] == right[j] ? 0 : 1
+  m.times.sum do |i|
+    left[i] == right[i] ? 0 : 1
   end
 end
+
+def search(s, n, m)
+  (1...n).each.all? do |i|
+    diff(s[i - 1], s[i], m) == 1
+  end
+end
+
+answer = s.sort!.permutation.any? do |p|
+  search(p, n, m)
+end
+
+puts answer ? 'Yes' : 'No'
