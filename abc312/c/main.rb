fd29a2c7e8d6eb1d66b02ee5f5cb326a14819b04
buty4649 <buty4649@gmail.com>
2023-08-19 20:57:20 +0900

    update

--- abc312/c/main.rb
+++ abc312/c/main.rb
@@ -3,17 +3,24 @@
 n, m = gets.split.map(&:to_i)
 
 # 大きい順に累積和を取っておく
-a = gets.split.map(&:to_i).tally.sort.reverse.to_a
+a = gets.split.map(&:to_i).tally.sort.to_a
 (a.size - 1).times do |i|
   a[i + 1][1] = a[i][1] + a[i + 1][1]
 end
 
 # 小さい順に累積和を取っておく
-b = gets.split.map(&:to_i).tally.sort.to_a
+b = gets.split.map(&:to_i).tally.sort.reverse.to_a
 (b.size - 1).times do |i|
   b[i + 1][1] = b[i][1] + b[i + 1][1]
 end
 
 a.reverse!
-b.reverse!
 pp({a:a, b:b})
+
+answer = 10**9
+
+b.each do |value, count|
+  buyer = a.find { |v, c| value >= v && count >= c }
+
+  pp buyer
+end
