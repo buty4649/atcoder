48484049b3d1d476398b033cce4f15b8ef7933a2
buty4649 <buty4649@gmail.com>
2023-09-02 21:00:54 +0900

    update

--- abc317/c/main.rb
+++ abc317/c/main.rb
@@ -2,37 +2,39 @@
 
 n,m = gets.split.map(&:to_i)
 
-map = Array.new(11) { Array.new(11, 0) }
+map = {}
 m.times do
   a,b,c = gets.split.map(&:to_i)
+  map[a] ||= {}
   map[a][b] = c
+  map[b] ||= {}
   map[b][a] = c
 end
 
 class Resolver
   def initialize(map)
     @map = map
-    @answer = 0
-    @check = [false] * 11
+    @cache = {}
   end
 
   def answer
-    (1..10).each do |i|
-      resolv(i, 0)
-    end
-    @answer
+    @map.keys.map do |town|
+      resolv(town, 0, 0)
+    end.max
   end
 
-  def resolv(town, cost)
-    @answer = cost if cost > @answer
+  def resolv(town, cost, check_bit)
+    # キャッシュにあればそれを返す
+    check_bit |= 1 << town
+    return @cache.dig(town, check_bit) if @cache.dig(town, check_bit)
 
-    @check[town] = true
-    (1..10).each do |i|
-      next if @check[i] || @map[town][i] == 0
+    max_cost = @map[town].map do |(dest, dest_cost)|
+      next cost if check_bit[dest].positive?
 
-      resolv(i, cost + @map[town][i])
-    end
-    @check[town] = false
+      resolv(dest, cost + dest_cost, check_bit)
+    end.max
+    @cache[town] ||= {}
+    @cache[town][check_bit] = max_cost
   end
 end
 
